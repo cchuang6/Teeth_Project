314eda7f05aa7b2d11aabd8191114eb434cca76d
unknown <cchuang6@ASUP4055454.poly.ad.asu.edu>
2013-11-12 17:51:36 -0700

    add dependency to virtual env, create an editor

---
+++ virtualenv/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\Users\cchuang6.ASURITE\Documents\NSF_Teeth\virtualenv\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
